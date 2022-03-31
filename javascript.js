721110db4dd333a3c6855c1e5501e0fb6e4b6395
Brynn <durantbrynn@gmail.com>
2022-03-31 11:15:44 -0500

    Added function to clear calculator display, and event listener for clear button to run function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+const clearButton = document.querySelector('button.clear');
 const add = function(...args) {
     let sum = 0;
     for (let i = 0; i < arguments.length;i++){
@@ -42,3 +43,12 @@ const operate = (a, b, operator) => {
         return divide(a,b);
     }
 }
+
+
+
+const clearDisplay = () => {
+    const currentDisplay = document.getElementById('display');
+    currentDisplay.value = "";
+}
+
+clearButton.addEventListener('click',clearDisplay);
